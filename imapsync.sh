67d39acc4010799c5d8c564a0b080accaaeea764
David Gitman <david@igniterate.com>
2023-04-12 19:04:20 -0400

    first commit

---
+++ imapsync.sh
@@ -0,0 +1,7 @@
+imapsync  \
+           --user1 davidmarkgitman@gmail.com \
+           --password1 ***REMOVED*** \
+           --user2 david@because.ventures \
+           --password2 ***REMOVED*** \
+           --gmail1 --gmail2 \
+           --folder "Because Ventures"   
