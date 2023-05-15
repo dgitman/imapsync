10147cac53c0498093d679edd3ff2e2c8798efaf
David Gitman <david@igniterate.com>
2023-05-15 12:17:26 -0400

    Added Anna Zickerman

--- imapsync.sh
+++ imapsync.sh
@@ -6,3 +6,12 @@ imapsync  \
            --gmail1 --gmail2 \
            --folder "Because Ventures" \
            --delete1
+
+imapsync  \
+           --user1 david@because.ventures \
+           --password1 ***REMOVED*** \
+           --user2 david@annazuckerman.com \
+           --password2 ***REMOVED*** \
+           --gmail1 --gmail2 \
+           --folder "Anna Zuckerman" \
+           --delete1
