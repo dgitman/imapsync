9db3c3daf030af2e26e398de4a7b8380749c61c2
David Gitman <david@igniterate.com>
2023-05-21 19:00:38 -0400

    added bklein

---
+++ imapsync-bklein.sh
@@ -0,0 +1,6 @@
+imapsync  \
+           --user1 drbarryklein@gmail.com \
+           --password1 ***REMOVED*** \
+           --user2 drklein@drbarryklein.com\
+           --password2 ***REMOVED*** \
+           --gmail1 --gmail2
